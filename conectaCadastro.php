8fb0a1d71abff4003c0c72a28dc33fc8abbedc13
Myke Oliveira <38540828+mykeoliveira@users.noreply.github.com>
2020-06-06 20:17:00 -0300

    Update conectaCadastro.php

--- conectaCadastro.php
+++ conectaCadastro.php
@@ -7,7 +7,12 @@ $login= $_POST ["login"];
 $senha= $_POST["senha"];
 
 //conectando com o localhost - mysqli
-$conexao = mysqli_connect("localhost","myke1","","REA") or die ('Não foi possível conectar');
+$dbname='heroku_7379346df1b396c';
+$usuario='b5270006d6b693';
+$password='5a57ec9b';
+$localhost='us-cdbr-east-05.cleardb.net';
+
+$conexao = mysqli_connect($localhost,$usuario,$password,$dbname) or die ('Não foi possível conectar');
 $query = "INSERT INTO `usuario` (`nome`,`login`,`senha`) VALUES ('$nome', '$login', '$senha')";
 //print $query;
 mysqli_query($conexao, $query) or die ("Erro no comando SQL:".mysqli_error());
