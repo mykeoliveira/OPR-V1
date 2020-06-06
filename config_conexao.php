2ffcb65fdaa1114200764ac21e4a6a1834c75978
Myke Oliveira <38540828+mykeoliveira@users.noreply.github.com>
2020-06-06 20:18:32 -0300

    Update config_conexao.php

--- config_conexao.php
+++ config_conexao.php
@@ -1,7 +1,8 @@
 ﻿<?php
 //Definendo variaveis de conexao com banco de dados
-define("SERVIDOR", "localhost");
-define("USUARIO", "myke1");
-define("SENHA", "");
-define("BANCO", "REA");
+define("SERVIDOR", "us-cdbr-east-05.cleardb.net");
+define("USUARIO", "b5270006d6b693");
+define("SENHA", "5a57ec9b");
+define("BANCO", "heroku_7379346df1b396c");
+
 ?> 
